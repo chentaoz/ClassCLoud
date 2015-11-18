91c98ba717ba0018ca501239e8f75e63a395a569
Chentao Zhang <chentaoz@udel.edu>
2015-11-17 23:56:26 -0500

    auth login dropbox

--- SpringServer/src/main/java/cloud/distrFileSys/master/controller/IndexController.java
+++ SpringServer/src/main/java/cloud/distrFileSys/master/controller/IndexController.java
@@ -1,8 +1,22 @@
 package cloud.distrFileSys.master.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.Users;
+
+import cloud.distrFileSys.support.service.Configuration;
+
+
 
 @Controller
 public class IndexController {
@@ -11,5 +25,31 @@ public class IndexController {
 	        model.addAttribute("greeting", "MVC framework construction successed!");
 	        return "index"; 
 	    }
+	  
+	  
+	 
+	  
+	  @RequestMapping(value = Configuration.LOG_IN)
+	  public String login(@PathVariable("id") long id,Model model) 
+			  throws Users.GetCurrentAccountException, DbxException 
+	  {
+		  
+		  String[] accessTokens=Configuration.ACCESS_TOKEN;
+		  
+		  List<DbxClientV2> clients=new ArrayList<DbxClientV2>();
+		  for(String at: accessTokens){
+			  DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
+			  DbxClientV2 client = new DbxClientV2(config, at);
+			  clients.add(client);
+		  }
+		 
+		  
+		  Users.FullAccount account = clients.get(0).users.getCurrentAccount();
+		  model.addAttribute("login", account.name.displayName);
+	      System.out.println(account.name.displayName);
+	        
+	      return "login";
+		  
+	  }
 	
 }
---
+++ SpringServer/src/main/java/cloud/distrFileSys/support/service/Configuration.java
@@ -0,0 +1,21 @@
+package cloud.distrFileSys.support.service;
+
+public class Configuration {
+	
+	public final static String UPLOAD_PROCESS_PATH="";//path to upload file on cloud provide
+	
+	public final static String UPLOAD_PATH_START="/upload/{id}";
+	
+	public final static String UPLOAD_PATH_END="/upload_done/{id}/session";
+	
+	public final static String GET_FILE_PATH="get/{path}";
+	
+	public final static String DOWNLOAD_PROCESS_PATH="";
+	
+	public final static String[] ACCESS_TOKEN={"qkh1oWRdV5AAAAAAAAAAGk4j2QG0QGzPs39We1axFe5DrASsWQiWj2yxy0jT-1cq"};
+	
+	public final static String LOG_IN="/login/{id}"; 
+	
+	
+
+}
